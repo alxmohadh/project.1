4775e52988ec6f0e7a9e104b0a3720e2b703022c
alxmohadh <amohamedahmed034@gmail.com>
2025-04-05 00:16:33 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        scanner.close();
-
         if (n > 2 && n % 2 == 0)
             System.out.println("YES");
         else
